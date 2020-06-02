c511efcfb8503db512fa1ac7d8168de3178bd552
Julian Blagis <julianbgs96@gmail.com>
2020-06-02 22:38:52 +0200

    add dropdown

--- src/pages/home/tasks/index.vue
+++ src/pages/home/tasks/index.vue
@@ -26,11 +26,11 @@
                     template(slot="singleLabel", slot-scope="props")
                       span(v-bind:style="{backgroundColor: props.option.color}").option__desc-active
                         img.option__image(:src="props.option.img", alt="icon image")
-                        span.option__title {{ props.option.title }}
+                        span(v-bind:style="{color: props.option.colorText}").option__title {{ props.option.title }}
                     template(slot="option", slot-scope="props")
                         .option__desc
                           img.option__image(:src="props.option.img", alt="icon image")
-                          span.option__title {{ props.option.title }}
+                          span(v-bind:style="{color: props.option.colorText}").option__title {{ props.option.title }}
                 multiselect(
                   v-model="valueArraySecond[index]",
                   placeholder="Ничего не выбрано",
@@ -43,11 +43,11 @@
                     template(slot="singleLabel", slot-scope="props")
                       span(v-bind:style="{backgroundColor: props.option.color}").option__desc-active
                         img.option__image(:src="props.option.img", alt="icon image")
-                        span.option__title {{ props.option.title }}
+                        span(v-bind:style="{color: props.option.colorText}").option__title {{ props.option.title }}
                     template(slot="option", slot-scope="props")
                       .option__desc
                         img.option__image(:src="props.option.img", alt="icon image")
-                        span.option__title {{ props.option.title }}
+                        span(v-bind:style="{color: props.option.colorText}").option__title {{ props.option.title }}
                 multiselect(
                   v-model="valueArrayThird[index]",
                   placeholder="Ничего не выбрано",
@@ -60,11 +60,11 @@
                     template(slot="singleLabel", slot-scope="props")
                       span(v-bind:style="{backgroundColor: props.option.color}").option__desc-active
                         img.option__image(:src="props.option.img", alt="icon image")
-                        span.option__title {{ props.option.title }}
+                        span(v-bind:style="{color: props.option.colorText}").option__title {{ props.option.title }}
                     template(slot="option", slot-scope="props")
                       .option__desc
                         img.option__image(:src="props.option.img", alt="icon image")
-                        span.option__title {{ props.option.title }}
+                        span(v-bind:style="{color: props.option.colorText}").option__title {{ props.option.title }}
             .tasks__issue-actions
               .tasks__issue-date
                 | 06.05.2020
@@ -191,58 +191,58 @@
       return {
         countArray: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         valueArrayFirst: [
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
-          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'В Работе', color: '#D9F5E6', colorText: '#32C979'},
         ],
         optionsFirst: [
-          { img: 'images/tasks/icon1.svg', title: 'Сделан', color: '#25B419'},
-          { img: 'images/tasks/icon1.svg', title: 'Пауза', color: '#F1F3F5' },
-          { img: 'images/tasks/icon1.svg', title: 'В работе', color: '#D9F5E6' },
-          { img: 'images/tasks/icon1.svg', title: 'Новый', color: '#F9E6D9' }
+          { img: 'images/tasks/icon1.svg', title: 'Сделан', color: '#25B419', colorText: '#065535'},
+          { img: 'images/tasks/icon1.svg', title: 'Пауза', color: '#F1F3F5' , colorText: '#93979B'},
+          { img: 'images/tasks/icon1.svg', title: 'В работе', color: '#D9F5E6' , colorText: '#32C979'},
+          { img: 'images/tasks/icon1.svg', title: 'Новый', color: '#F9E6D9', colorText: '#FF8330' }
         ],
         valueArraySecond: [
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5', colorText: '#93979B'},
         ],
         optionsSecond: [
-          { img: 'images/tasks/icon2.svg', title: 'Владислав', color: '#D9F5E6'},
-          { img: 'images/tasks/icon2.svg', title: 'Дмитрий', color: '#25B419' },
-          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5' },
-          { img: 'images/tasks/icon2.svg', title: 'Артур', color: '#F9E6D9' }
+          { img: 'images/tasks/icon2.svg', title: 'Владислав', color: '#D9F5E6', colorText: '#32C979'},
+          { img: 'images/tasks/icon2.svg', title: 'Дмитрий', color: '#25B419', colorText: '#065535' },
+          { img: 'images/tasks/icon2.svg', title: 'Константин', color: '#F1F3F5' , colorText: '#93979B'},
+          { img: 'images/tasks/icon2.svg', title: 'Артур', color: '#F9E6D9', colorText: '#FF8330' }
         ],
         valueArrayThird: [
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9', colorText: '#FF8330'},
         ],
         optionsThird: [
-          { img: 'images/tasks/icon3.svg', title: 'Низкий', color: '#F1F3F5'},
-          { img: 'images/tasks/icon3.svg', title: 'Средне', color: '#25B419' },
-          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9' },
-          { img: 'images/tasks/icon3.svg', title: 'Высокий', color: '#D9F5E6' }
+          { img: 'images/tasks/icon3.svg', title: 'Низкий', color: '#F1F3F5', colorText: '#93979B'},
+          { img: 'images/tasks/icon3.svg', title: 'Средне', color: '#25B419' , colorText: '#065535'},
+          { img: 'images/tasks/icon3.svg', title: 'Максимум', color: '#F9E6D9' , colorText: '#FF8330'},
+          { img: 'images/tasks/icon3.svg', title: 'Высокий', color: '#D9F5E6', colorText: '#32C979' }
         ],
         valueFilterFirst: {title: 'Проект'},
         optionFilterFirst: [
